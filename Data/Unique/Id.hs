64a7c8d657e3ed91267513d0f58af8f5d075eecf
Sebastian Fischer <sebf@informatik.uni-kiel.de>
2008-12-18 17:29:22 +0100

    haddock comments.

--- Data/Unique/Id.hs
+++ Data/Unique/Id.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE MagicHash #-}
 
+-- | This module provides splittable supplies for unique identifiers.
+-- | The main idea gows back to L. Augustsson, M. Rittri, and D. Synek
+-- | and is described in their paper "On generating unique names"
+-- | (Journal of Functional Programming 4(1), 1994. pp. 117-123). The
+-- | implementation at hand is taken from the GHC sources and includes
+-- | bit fiddling to allow multiple supplies that generate unique
+-- | identifiers by prepending a character given at initialization.
 module Data.Unique.Id (
 
   Id, hashedId, IdSupply, initIdSupply, splitIdSupply, idFromSupply
@@ -12,10 +19,16 @@ import GHC.IOBase ( unsafeDupableInterleaveIO )
 import Data.IORef
 import System.IO.Unsafe ( unsafePerformIO )
 
+-- | Unique identifiers are of type 'Id' and can be hashed to an 'Int'
+-- | usning the function 'hashedId'.
 newtype Id = Id { hashedId :: Int }
 
+-- | Supplies for unique identifiers are of type 'IdSupply' and can be
+-- | split into two new supplies or yield a unique identifier.
 data IdSupply = IdSupply Int# IdSupply IdSupply
 
+-- | Generates a new supply of unique identifiers. The given character
+-- | is prepended to the generated numbers.
 initIdSupply :: Char -> IO IdSupply
 initIdSupply (C# c) =
  case uncheckedIShiftL# (ord# c) (unboxedInt 24) of
@@ -28,9 +41,11 @@ initIdSupply (C# c) =
          return (IdSupply (word2Int# (or# (int2Word# mask) (int2Word# u))) l r))
     in mkSupply
 
+-- | Splits a supply of unique identifiers to yield two of them.
 splitIdSupply :: IdSupply -> (IdSupply,IdSupply)
 splitIdSupply (IdSupply _ l r) = (l,r)
 
+-- | Yields the unique identifier from a supply.
 idFromSupply :: IdSupply -> Id
 idFromSupply (IdSupply n _ _) = Id (I# n)
 
